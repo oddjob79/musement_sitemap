5303391b6c3fbea35f8dc6e75080970e92906dff
engelbert <28482378+oddjob79@users.noreply.github.com>
2020-01-07 15:40:27 +0100

    feat: Added getLinks function to retrieve all links from html content

--- public/curl1.php
+++ public/curl1.php
@@ -24,20 +24,53 @@
     // $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     // $url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
     // $filetime = curl_getinfo($ch,  CURLINFO_FILETIME);
-    $pageinfo = curl_getinfo($ch);
+    $info = curl_getinfo($ch);
 
     // close curl resource to free up system resources
     curl_close($ch);
 
-    return $pageinfo;
+    return array('info'=>$info, 'content'=>$output);
     // echo ($output);
   }
+
+  function getLinks($content) {            // adapted from example on PHP.NET/manual given by Jay Gilford
+
+      // Create a new DOM Document to hold our webpage structure
+      $xml = new DOMDocument();
+
+      // set error level
+      $internalErrors = libxml_use_internal_errors(true);
+
+      // Load the url's contents into the DOM
+      $xml->loadHTML($content);
+
+      // Restore error level
+      libxml_use_internal_errors($internalErrors);
+
+      // Empty array to hold all links to return
+      $links = array();
+
+      //Loop through each <a> tag in the dom and add it to the link array
+      foreach($xml->getElementsByTagName('a') as $link) {
+        if (substr($link->getAttribute('href'), 0, 7) != 'mailto:' && substr($link->getAttribute('href'), 0, 4) != 'tel:') {
+          // $links[] = array('url' => $link->getAttribute('href'));
+          array_push($links, $link->getAttribute('href'));
+          // $links[] = array('url' => $link->getAttribute('href'), 'text' => $link->nodeValue);
+        }
+      }
+
+      //Return the links
+      return $links;
+  }
+
 
   // call function to retrieve page data
   $target = "https://www.musement.com/es/";
   // $target = "https://www.php.net/manual/en/function.explode.php"; // example url with last modified time in header
 
-  $pageinfo = getPageData($target);
+  $res = getPageData($target);
+  $pageinfo = $res['info'];
+  $content = $res['content'];
 
   echo 'HTTP CODE = ', $pageinfo['http_code']; // $http_code;
   echo '<br />URL = ', $pageinfo['url']; // $url;
@@ -46,6 +79,11 @@
   } else {
     echo '<br />Filetime = None';
   }
+
+  $linklist = getLinks($content);
+  echo '<br />Links: ';
+  var_dump($linklist);
+  // echo '<br />Content = ', $content;
 
 
 
