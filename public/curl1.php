c01e277ec7b214096f2f68c8d5ec23cc3d2799a3
engelbert <28482378+oddjob79@users.noreply.github.com>
2020-01-08 12:20:15 +0100

    feat: Add functionality to loop through URLs
    Also added a list of scanned urls to compare with.

--- public/curl1.php
+++ public/curl1.php
@@ -36,10 +36,13 @@
   function relativeToAbsoluteLinks($rellinks) {
     $abslinks = array();
     foreach ($rellinks as $link) {
+      // if absolute link then leave as is
       if (substr($link, 0, 4) == 'http') {
         array_push($abslinks, $link);
+      // if relative link then add protocol and domain
       } else {
-        array_push($abslinks, 'https://www.musement.com'.$link);
+        // array_push($abslinks, 'https://www.musement.com'.$link);
+        array_push($abslinks, 'http://books.toscrape.com/'.$link);
       }
     }
     return $abslinks;
@@ -61,48 +64,111 @@
       libxml_use_internal_errors($internalErrors);
 
       // Empty array to hold all links to return
-      $links = array();
+      $pagelinks = array();
 
       //Loop through each <a> tag in the dom and add it to the link array
       foreach($xml->getElementsByTagName('a') as $link) {
+        // if link is a mailto link or a tel link - ignore it
         if (substr($link->getAttribute('href'), 0, 7) != 'mailto:' && substr($link->getAttribute('href'), 0, 4) != 'tel:') {
           // $links[] = array('url' => $link->getAttribute('href'));
-          array_push($links, $link->getAttribute('href'));
+          array_push($pagelinks, $link->getAttribute('href'));
           // $links[] = array('url' => $link->getAttribute('href'), 'text' => $link->nodeValue);
         }
       }
 
       // convert all relative links to absolute
-      $links = relativeToAbsoluteLinks($links);
-
-
+      $pagelinks = relativeToAbsoluteLinks($pagelinks);
 
       //Return the links
-      return $links;
+      return $pagelinks;
   }
 
+  function writeData($pageinfo, $scannedurls) {
+    // echo sitemap information
+    echo 'HTTP CODE = ', $pageinfo['http_code']; // $http_code;
+    echo '<br />URL = ', $pageinfo['url']; // $url;
+    if ($pageinfo['filetime'] != -1) {
+      echo '<br />Filetime = ', date("Y-m-d H:i:s", $pageinfo['filetime']); // date("Y-m-d H:i:s", $filetime);
+    } else {
+      echo '<br />Filetime = None';
+    }
+    echo '<br />';
+
+    array_push($scannedurls, $pageinfo['url']);
+
+    return $scannedurls;
+  }
+
+  function scanURLs($masterlinks, $scannedurls) {
+    // limit $masterlinks for testing
+    $masterlinks = array_slice($masterlinks, 0, 10);
+    // for each url in masterlinks
+    foreach ($masterlinks as $link) {
+      if (!in_array($link, $scannedurls)) {
+        // use curl to get pgae data
+        $res = getPageData($link);
+        // separate into page content (for links) and page info (for sitemap)
+        $pageinfo = $res['info'];
+        $pagecontent = $res['content'];
+
+        $scannedurls = writeData($pageinfo, $scannedurls);
+
+        // GET NEW LINKS
+        // generate list of links from page content
+        $linklist = getLinks($pagecontent);
+        // merge list of links returned from page with "master" list of links
+        $masterlinks = array_merge($masterlinks, $linklist);
+        // remove dupes
+        $masterlinks = array_unique($masterlinks);
+      }
+    }
+    return array('newlinks'=>$masterlinks, 'scanned'=>$scannedurls);
+  }
 
   // call function to retrieve page data
-  $target = "https://www.musement.com/es/";
+  // $target = "https://www.musement.com/es/";
+  $target = "http://books.toscrape.com/";
   // $target = "https://www.php.net/manual/en/function.explode.php"; // example url with last modified time in header
 
-  $res = getPageData($target);
-  $pageinfo = $res['info'];
-  $content = $res['content'];
+  // $res = getPageData($target);
+  // $pageinfo = $res['info'];
+  // $pagecontent = $res['content'];
+  //
+  // echo 'HTTP CODE = ', $pageinfo['http_code']; // $http_code;
+  // echo '<br />URL = ', $pageinfo['url']; // $url;
+  // if ($pageinfo['filetime'] != -1) {
+  //   echo '<br />Filetime = ', date("Y-m-d H:i:s", $pageinfo['filetime']); // date("Y-m-d H:i:s", $filetime);
+  // } else {
+  //   echo '<br />Filetime = None';
+  // }
+  // echo '<br />';
+  // // generate list of links from page content
+  // $linklist = getLinks($pagecontent);
+  // // merge list of links returned from page with "master" list of links
+  // $masterlinks = array_merge($masterlinks, $linklist);
+  // // remove dupes
+  // $masterlinks = array_unique($masterlinks);
+  // // echo '<br />Links: ';
+  // // var_dump($linklist);
+  // // echo '<br />Content = ', $content;
 
-  echo 'HTTP CODE = ', $pageinfo['http_code']; // $http_code;
-  echo '<br />URL = ', $pageinfo['url']; // $url;
-  if ($pageinfo['filetime'] != -1) {
-    echo '<br />Filetime = ', date("Y-m-d H:i:s", $pageinfo['filetime']); // date("Y-m-d H:i:s", $filetime);
-  } else {
-    echo '<br />Filetime = None';
+  $masterlinks = array($target);
+  $scannedurls = array();
+  $i=0;
+
+  while (!empty(array_diff($masterlinks, $scannedurls)) && $i<2) {
+    echo '<br />Loop number '.$i.'<br />';
+    $output = scanURLs($masterlinks, $scannedurls);
+    $masterlinks = $output['newlinks'];
+    $scannedurls = $output['scanned'];
+    $i++;
   }
 
-  $linklist = getLinks($content);
-  echo '<br />Links: ';
-  var_dump($linklist);
-  // echo '<br />Content = ', $content;
+  echo '<br />New Links:<br />';
+  var_dump($masterlinks);
 
+  echo '<br />Scanned URLs:<br />';
+  var_dump($scannedurls);
 
 
 ?>
